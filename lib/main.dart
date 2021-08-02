fb7cf4d3d07fc3e901051588b145df523fa7972e
Garmode3073 <gauravgarmode16@gmail.com>
2021-08-02 15:57:06 +0530

    firebase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,41 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:mpdemo/login.dart';
+import 'package:mpdemo/widgets/loading.dart';
+import 'package:mpdemo/screens/login.dart';
 import 'package:mpdemo/global.dart' as g;
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: g.primary,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(),
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        // if (snapshot.hasError) {
+        //   return SomethingWentWrong();
+        // }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: 'Flutter Demo',
+            theme: ThemeData(
+              primarySwatch: g.primary,
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+            ),
+            home: MyHomePage(),
+          );
+        }
+        // Otherwise, show something whilst waiting for initialization to complete
+        return LoadingPage();
+      },
     );
   }
 }
@@ -31,6 +51,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return LoginPage();
+    return LoadingPage();
   }
 }
